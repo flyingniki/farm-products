2c23280978ffc0ff360f00cecea4203162a16990
Petr Nikitin <flying_niki@mail.ru>
2023-06-19 13:08:03 +0300

    step 10 done

--- src/components/pages/order/order.jsx
+++ src/components/pages/order/order.jsx
@@ -19,10 +19,35 @@ import "swiper/swiper-bundle.min.css";
 SwiperCore.use([Mousewheel, Pagination, Scrollbar]);
 
 // Оформление заказа
-function Order({ products }) {
+function Order({
+  products, // список продуктов
+}) {
+  const [swiperRef, setSwiperRef] = useState(null);
   const [selectProductIds, setSelectProductIds] = useState([]);
-
-  return (
+  //id в продукты
+  const selectProducts = selectProductIds.map((id) =>
+    products.find((product) => product.id === id)
+  );
+  //цена покупки
+  const fullPrice = selectProducts.reduce(
+    (sum, product) => (sum += product.price),
+    0
+  );
+  const handleOnClickProduct = (value, index) => {
+    if (!selectProductIds.includes(value)) {
+      swiperRef.slideTo(index, 0);
+    }
+  };
+  const [address, setAddress] = useState("");
+  const handleBuyClick = () => {
+    // eslint-disable-next-line no-alert
+    alert(`Спасибо за заказ, вы купили:\n${selectProducts.map(
+      (product) => `${product.name} - ${product.price} руб.\n`
+    )}
+    Итого: ${fullPrice} руб.
+    Доставка по адресу: ${address}.`);
+  };
+  return products && products.length ? (
     <StyledOrder as="form">
       <LeftColumn>
         <Panel marginBottom={20} paddingTop={24} paddingBottom={10}>
@@ -39,26 +64,38 @@ function Order({ products }) {
             }))}
             selectValues={selectProductIds}
             onChange={setSelectProductIds}
+            onClickLabel={handleOnClickProduct}
           />
         </Panel>
         <Panel>
           <Title size={TitleSize.EXTRA_SMALL} marginBottom={24}>
             Сделать заказ
           </Title>
-          <AddressInput placeholder="Введите адрес доставки" />
+          <AddressInput
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            placeholder="Введите адрес доставки"
+          />
           <PriceLabel as="span">Цена</PriceLabel>
-          <PriceValue>400</PriceValue>
-          <Button maxWidth>Купить</Button>
+          <PriceValue value={fullPrice} />
+          <Button
+            maxWidth
+            onClick={handleBuyClick}
+            disabled={!(selectProductIds.length && address)}
+          >
+            Купить
+          </Button>
         </Panel>
       </LeftColumn>
       <ProductSwiper
+        onSwiper={setSwiperRef}
         spaceBetween={12}
         direction="vertical"
         slidesPerView="auto"
         scrollbar={{ draggable: true }}
         mousewheel
         pagination={{
-          type: "fraction",
+          type: "fanction",
         }}
       >
         {products.map((product) => (
@@ -68,6 +105,8 @@ function Order({ products }) {
         ))}
       </ProductSwiper>
     </StyledOrder>
+  ) : (
+    "Продукты были слишком вкусные и их разобрали."
   );
 }
 
